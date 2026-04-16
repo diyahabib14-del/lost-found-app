09856b908e17468d6748364a6b3026dc765ffd12
Diya <diyahbib14@gmail.com>
2026-04-17 00:45:04 +0530

    fixed image upload preset

--- frontend/script.js
+++ frontend/script.js
@@ -12,7 +12,7 @@ document.getElementById("itemForm").addEventListener("submit", async (e) => {
         const formData = new FormData();
         formData.append("file", file);
         formData.append("upload_preset", "vwc2caen");
-
+console.log("UPLOAD DATA:", data);
         const res = await fetch("https://api.cloudinary.com/v1_1/dqw3uggee/image/upload", {
             method: "POST",
             body: formData
